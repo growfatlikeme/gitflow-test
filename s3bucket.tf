0ae17ff21b6f4a68e615596fd84efd3bc938610d
GrowFatLikeMe <freakishcrap@gmail.com>
2025-07-08 21:25:25 +0800

    Added terraform files and workflow

---
+++ s3bucket.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "estee-module3.tfstate" #The name of the file in the bucket
+    region = "ap-southeast-1"  
+  }
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "growfat-bucket"                        #Use a globally unique name
+  force_destroy = true
+}
